5bc33366cf1279aadf831fb233a4188fa32ac7f5
Nitzan Cohen <nitzancohen@hotmail.com>
2024-02-15 13:59:00 +0200

    Fixed bugs

--- src/components/LearningManagementSystem/LessonTitle/LessonTitle.tsx
+++ src/components/LearningManagementSystem/LessonTitle/LessonTitle.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ModuleCourse } from "@/app/courses/[...lms]/page";
+import { ModuleCourse } from "@/types/coursesTypes";
 import { Flex, Heading, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
